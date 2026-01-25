3e5687e9747e238f04505b5218c6868ac039d1b0
Santosh Pujeri <16santoshpujari@gmail.com>
2026-01-26 01:55:21 +0530

    Add REST controller integration tests and centralized exception handling

---
+++ src/test/java/dev/santosh/productservice/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package dev.santosh.productservice.controllers;
+
+public class GlobalExceptionHandler {
+}
---
+++ src/test/java/dev/santosh/productservice/controllers/ProductsControllerIT.java
@@ -0,0 +1,5 @@
+package dev.santosh.productservice.controllers;
+
+class ProductsControllerIT {
+
+}
